cae53d65e76f95c315b314d144feacc31cdd9b48
masha1907 <gottmann.MG@googlemail.com>
2022-10-23 13:58:44 +0200

    add Produkt

---
+++ Script (1).js
@@ -0,0 +1,12 @@
+db.products.insertOne({
+    
+      'id': 'ng1278bg679',
+      'category': 'TV & Audio',
+      'size' : '75 zoll',
+      'brand': 'SONY',
+      'display technology' : 'LED',
+      'resolution': '4K UHD',
+      'special feature' : 'SMART TV',
+      'price': '1500',
+}
+)
