cf796e23defc56069f1fe3c9cab395ed154cea29
masha1907 <gottmann.MG@googlemail.com>
2023-01-17 16:57:23 +0100

    post_employee_order

---
+++ MG.DB/HW.py
@@ -0,0 +1,48 @@
+post = {
+    'id': 'it_09',
+    'topic': 'Тема поста',
+    'author_id': '019',
+    'tags': ['java', 'mongodb'],
+    'content': ['text', 'pictures', 'diagrams'],
+    'datetime': '......',
+    'is_approved': 'true',
+    'post_status': 'publish',
+    'post_type': 'post',
+    'comment_status': 'open',
+    'post_password': 'asdhjfn09'
+}
+
+employee = {
+    'employee_id': '018',
+    '_id': 'm45',
+    'name': 'Robert Schwarz',
+    'b_data': '10_03_1987',
+    'position': 'manager',
+    'contact': [{
+        'mobile': '0178-9874653'
+    },
+        {
+        'adresse': {
+            'Country': 'Germany',
+            'City': 'Berlin',
+            'Street': 'Bahnhofstrasse',
+            'House_number': '34'
+        }
+    },
+        {
+        'email': ['marun@example.org', 'berlin@test.com']
+    }],
+    'salary': 3876,
+}
+
+order = {
+    'order_id': 'gh076',
+    'product_id': 'gu065',
+    'is_paid': 'true',
+    'datetime': {
+        'created': '.....',
+        'paid': '......'},
+    'client_id': 'gt045',
+    'provider_id': 'gj098',
+    'quantity': '1'
+}
