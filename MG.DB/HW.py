3f8dbb21a3d1fcffd5de966732b5d6cb74908ffb
masha1907 <gottmann.MG@googlemail.com>
2023-01-18 08:52:55 +0100

    revised

--- MG.DB/HW.py
+++ MG.DB/HW.py
@@ -3,46 +3,45 @@ post = {
     'topic': 'Тема поста',
     'author_id': '019',
     'tags': ['java', 'mongodb'],
-    'content': ['text', 'pictures', 'diagrams'],
+    'content': ['text', 'pictures'],
     'datetime': '......',
-    'is_approved': 'true',
-    'post_status': 'publish',
-    'post_type': 'post',
+    'is_approved': True,
+    'status': 'publish',
+    'type': 'post',
     'comment_status': 'open',
     'post_password': 'asdhjfn09'
 }
 
 employee = {
     'employee_id': '018',
-    '_id': 'm45',
-    'name': 'Robert Schwarz',
+    'division_id': 'm45',
+    'full_name': 'Robert Schwarz',
     'b_data': '10_03_1987',
     'position': 'manager',
-    'contact': [{
-        'mobile': '0178-9874653'
-    },
-        {
-        'adresse': {
+    'contact': {
+        'mobile': '0178-9874653',
+        'address': {
             'Country': 'Germany',
             'City': 'Berlin',
             'Street': 'Bahnhofstrasse',
             'House_number': '34'
-        }
-    },
-        {
-        'email': ['marun@example.org', 'berlin@test.com']
-    }],
+        },
+        'email': ['marun@example.org', 'berlin@test.com']},
     'salary': 3876,
 }
 
 order = {
     'order_id': 'gh076',
-    'product_id': 'gu065',
-    'is_paid': 'true',
+    'is_paid': True,
     'datetime': {
         'created': '.....',
         'paid': '......'},
     'client_id': 'gt045',
-    'provider_id': 'gj098',
-    'quantity': '1'
+    'quantity': 1,
+    'products': [
+        {'id': 'g5', 'price': 450, 'q': 1, 'name': 'monitor'},
+        {'id': 'g6', 'price': 300, 'q': 1, 'name': 'printer'},
+        {'id': 'g9', 'price': 500, 'q': 1, 'name': 'graphics_card'}
+
+    ]
 }
